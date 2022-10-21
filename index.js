76f3a809e840a283750a298cf18fe98392a197f8
diego <guatemaladiegojose@gmail.com>
2022-10-21 13:00:04 -0400

    commit tree 8e4ae70ecd1e7db4be2984df4e58e1521d0d7090

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun (e="roller-skate"){
+    
+return (`This Saturday, I want to ${e}!`);
+}
+
+function mondayWork (t= "go to the office") {
+    return (`This Monday, I will ${t}.`);
+}
+
+function wrapAdjective(star= "*"){
+    const inner = function (defu = "special") {
+        return `You are ${star}${defu}${star}!`;
+    };
+    return inner;
+}
+
+
+
+
---
+++ test/index.js
@@ -0,0 +1,8 @@
+// code your solution here
+
+function saturdayFun (String){
+return (`This Saturday, I want to ${String}!`);
+
+
+}
+
